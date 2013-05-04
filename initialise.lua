47501abb81077a53820435c484eaf71a97b4bebe
andy <andy@stormbase.net>
2013-05-04 18:38:37 +0100

    config: copy tables together rather than use metas

--- initialise.lua
+++ initialise.lua
@@ -1,125 +1,73 @@
 local addon, ns = ...
 local config = ns.config
 
-local combineAuraTables = function(base, specific)
-	
-	local output = {}
+local function combineTables(t1, t2)
 
-	for k,v in ipairs(base) do
-		output[v] = true
+	local out = {}
+
+	for k,value in pairs(t1) do
+
+		if type(value) == "table" then
+			out[k] = combineTables(value, t2[k] or {})
+		else
+			out[k] = value 	
+		end
+
 	end
 
-	for k,v in ipairs(specific) do
-		output[v] = true
-	end
-
-	output.mode = specific.mode or base.mode
-	output.filter = specific.filter or base.filter
-
-	return output
-
-end
-
-local combineControllers = function(base, specific)
-	
-	local combined = {}
-
-	for i,v in ipairs(base) do
-		combined[v] = true
-	end
-
-	for i,v in ipairs(specific) do
-		combined[v] = true
-	end
-
-	local output = {}
-
-	for k,v in pairs(combined) do
-		table.insert(output, k)
-	end
-
-	return output
-
-end
-
-local processAuraConfig = function(specConfig)
-
-	local baseAuraConfig = config.specBase.auras
-	local auraConfig = specConfig.auras
-
-	--handle unspecified units
-	setmetatable(auraConfig, { __index = config.specBase.auras })
-
-	auraConfig.player = combineAuraTables(baseAuraConfig.player, auraConfig.player)
-	auraConfig.target = combineAuraTables(baseAuraConfig.target, auraConfig.target)
-	auraConfig.focus = combineAuraTables(baseAuraConfig.focus, auraConfig.focus)	
-
-end
-
-local processAlertConfig = function(specConfig)
-
-	local alerts = specConfig.alerts
-	local base = config.specBase.alerts
-
-	for displayName, items in pairs(alerts) do
+	for k,value in pairs(t2) do
 		
-		local baseControllers = base[displayName].controllers
+		if type(value) == "table" then
+			out[k] = combineTables(t1[k] or {}, value)
+		else
+			out[k] = value
+		end
 
-		for i, alert in ipairs(items) do
+	end
+
+    return out
+
+end
+
+local convertToHash = function(input)
+	
+	for i,v in ipairs(input) do
+		input[v] = true
+	end
+
+end
+
+local combineSpecTablesWithBase = function()
+
+	local base = ns.config.specBase
+
+	convertToHash(base.auras.player)
+	convertToHash(base.auras.focus)
+	convertToHash(base.auras.target)
+	
+	--use base data for unspecified classes
+	config.classConfig.default = {}
+	setmetatable(config.classConfig, { __index = function(t) return t.default end })
+
+	for className, classSpecs in pairs(config.classConfig) do
+		
+		--use base data for unspecified specs
+		setmetatable(classSpecs, { __index = base })
+
+		--rollup base data into spec data
+		for specName, specConfig in pairs(classSpecs) do
 			
-			alert.controllers = combineControllers(baseControllers, alert.controllers or {})
+			classSpecs[specName] = combineTables(base, specConfig)
 
 		end
 
 	end
-	--[[
-	local baseAlertConfig = config.specBase.alerts
-	
-
-	for name, alertDisplay in pairs(alerts) do
-		
-		local baseControllers = baseAlertConfig[name].controllers
-
-		if baseControllers then
-
-			for i, alert in pairs(alertDisplay) do
-				print(alert.type)
-			end
-		end
-
-	end
-	]]
 
 end
 
 local processConfig = function()
 	
-	-- use a default value for unspecified classes
-	config.classConfig.default = {}
-
-	setmetatable(config.classConfig, { __index = function(t, k) 
-		return t.default 
-	end })
-
-	for class, classConfig in pairs(config.classConfig) do
-
-		--handle unspecified specs
-		setmetatable(classConfig, { __index = function(t, k)
-			return config.specBase
-		end })
-
-
-		for spec, specConfig in pairs(classConfig) do
-		
-			--handle unspecified elements
-			setmetatable(specConfig, { __index = config.specBase })
-
-			processAuraConfig(specConfig)
-			processAlertConfig(specConfig)
-
-		end		
-
-	end
+	combineSpecTablesWithBase()	
 
 end
 
