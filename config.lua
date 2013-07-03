d3a054b24f7327aeac3a101fb51fa724c91b1248
andy <andy@stormbase.net>
2013-07-04 00:46:55 +0100

    config: added deathknight.frost and paladin.retribution

--- config.lua
+++ config.lua
@@ -53,12 +53,13 @@ ns.config.classConfig = {
 		["Unholy"] = {
 			alerts = {
 				rotation = {
-					{ type = "spell", 	args = 130736, 	controllers = { "healthGlow", "cdAndActiveText" }, 	extra = { unit="target", percent=35}}, 	-- soul reaper (unholy)
+					{ type = "spell", 	args = 130736, 	controllers = { "healthGlow", "cdAndActiveText" }, 	extra = { unit="target", percent=45}}, 	-- soul reaper (unholy)
 					{ type = "spell", 	args = 114851,	controllers = { "stacksGlow", "stacksText" }, 		extra = {stacks = 5}}, --blood tap
 					{ type = "spell", 	args = 81340, }, 		-- sudden doom
 					{ type = "spell", 	args = 91342,	controllers = { "stacksGlow", "stacksText" }, 		extra = {stacks = 5}}, --shadow infusion
 				},
 				cooldowns = {
+					{ type = "spell",	args = 47568 },			-- Empower Rune Weapon
 					{ type = "spell",	args = 49016 },			-- unholy frenzy
 					{ type = "spell",	args = 20572 },			-- blood fury (attack power)
 					{ type = "item",	args = {10, 96229} },	-- gloves, synapse springs (strength)
@@ -81,6 +82,34 @@ ns.config.classConfig = {
 				pet = {
 					--91342,		-- shadow infusion
 					63560,		-- dark transformation
+				}
+			},
+		},
+		["Frost"] = {
+			alerts = {
+				rotation = {
+					{ type = "spell", 	args = 130736, 	controllers = { "healthGlow", "cdAndActiveText" }, 	extra = { unit="target", percent=45}}, 	-- soul reaper (unholy)
+					{ type = "spell", 	args = 114851,	controllers = { "stacksGlow", "stacksText" }, 		extra = {stacks = 5}}, --blood tap
+					{ type = "spell", 	args = 51124,	},		-- killing machine
+					{ type = "spell", 	args = 59052,	},		-- rime
+				},
+				cooldowns = {
+					{ type = "spell",	args = 51271 },			-- pillar of frost
+					{ type = "spell",	args = 77575 },			-- outbreak
+					{ type = "item",	args = {10, 96229} },	-- gloves, synapse springs (strength)
+				},
+			},
+			auras = {
+				player = {
+					53365,		-- unholy strength (rune of the fallen crusader)
+					51271,		-- pillar of frost
+					126657,		-- Darkmist Vortex trinket
+					138702,		-- Shadow pan assult
+					126582,		-- unwavering might (Lei Shen's Final Orders)
+					138870,		-- Primordius' Talisman of Rage
+					105706,		-- Potion of Mogu Power
+					138759,		-- feather
+					48792,		-- ibf
 				}
 			},
 		},
@@ -131,6 +160,14 @@ ns.config.classConfig = {
 				}
 			}
 		},
+
+		["Retribution"] = {
+			auras = {
+				player = {
+					84963, 		-- Inquisition
+				},
+			},
+		},
 	},
 
 	PRIEST = {
@@ -150,7 +187,7 @@ ns.config.classConfig = {
 		["Enhancement"] = {
 			alerts = {
 				cooldowns = {
-					{ type = "spell", 	args = 2062 },			--earth elemental
+					--{ type = "spell", 	args = 2062 },			--earth elemental
 					{ type = "spell", 	args = 120668 },		--stormlash
 					{ type = "spell", 	args = 2894 },			--fire elemental
 					{ type = "spell", 	args = 51533 },			--feral spirit
