b1400e679b1caf47d9304387021eef9502a263de
andy <andy@andydote.co.uk>
2014-10-16 23:03:20 +0100

    config.warrior: remove deleted spells

--- config/classes.warrior.lua
+++ config/classes.warrior.lua
@@ -5,16 +5,12 @@ local config = {
 		alerts = {
 			cooldowns = {
 				{ type = "spell", 	args = 18499 },			-- berserker rage
-				{ type = "spell", 	args = 114207 },		-- Skull Banner
 				{ type = "spell", 	args = 107574 },		-- avatar
 				{ type = "spell", 	args = 1719 },			-- recklessness
 			},
 			rotation = {
-				--{ type = "spell", 	args = 18499 },			-- berserker rage
-				{ type = "spell", 	args = 7384 },			-- overpower
 				{ type = "spell", 	args = 12294 },			-- mortal strike
 				{ type = "spell", 	args = 86346 },			-- colossus smash
-				--{ type = "spell", 	args = 1719 },			-- recklessness	
 			},
 		},
 		auras = {
