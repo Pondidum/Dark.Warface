d4957346130c9aad79970d2d54db979cb4c4588e
andy <andy@andydote.co.uk>
2015-02-12 20:32:02 +0000

    init: whitespace

--- initialise.lua
+++ initialise.lua
@@ -2,7 +2,7 @@ local addon, ns = ...
 local config = ns.config
 
 local convertToHash = function(input)
-	
+
 	for i,v in ipairs(input) do
 		input[v] = true
 	end
@@ -18,13 +18,13 @@ local function combineTables(t1, t2)
 		if type(value) == "table" then
 			out[k] = combineTables(value, t2[k] or {})
 		else
-			out[k] = value 	
+			out[k] = value
 		end
 
 	end
 
 	for k,value in pairs(t2) do
-		
+
 		if type(value) == "table" then
 			out[k] = combineTables(t1[k] or {}, value)
 		else
@@ -40,13 +40,13 @@ end
 local combineSpecTablesWithBase = function()
 
 	local base = ns.config.specBase
-	
+
 	--use base data for unspecified classes
 	config.classConfig.default = {}
 	setmetatable(config.classConfig, { __index = function(t) return t.default end })
 
 	for className, classSpecs in pairs(config.classConfig) do
-		
+
 		--use base data for unspecified specs
 		setmetatable(classSpecs, { __index = function() return base end })
 
@@ -54,7 +54,7 @@ local combineSpecTablesWithBase = function()
 		for specName, specConfig in pairs(classSpecs) do
 
 			classSpecs[specName] = combineTables(base, specConfig)
-			
+
 		end
 
 	end
@@ -64,7 +64,7 @@ end
 local preProcessAuras = function()
 
 	local hashAuras = function(set)
-		
+
 		for k,v in pairs(set) do
 			convertToHash(v)
 		end
@@ -75,7 +75,7 @@ local preProcessAuras = function()
 
 	for className, classSpecs in pairs(config.classConfig) do
 		for specName, specConfig in pairs(classSpecs) do
-			
+
 			if specConfig.auras then
 				hashAuras(specConfig.auras)
 			end
@@ -86,7 +86,7 @@ local preProcessAuras = function()
 end
 
 local preProcessAlerts = function()
-	
+
 	for displayName, displayConfig in pairs(config.specBase.alerts) do
 
 		if displayConfig.controllers then
@@ -98,37 +98,37 @@ local preProcessAlerts = function()
 end
 
 local postProcessAlerts = function()
-	
+
 	for className, classSpecs in pairs(config.classConfig) do
 		for specName, specConfig in pairs(classSpecs) do
-			
+
 			for displayName, displayConfig in pairs(specConfig.alerts) do
-				
+
 				if displayConfig.controllers then
 					convertToHash(displayConfig.controllers)
 				end
 
 				for i, entry in pairs(displayConfig) do
-					
+
 					if entry.controllers then
 						convertToHash(entry.controllers)
 					end
-					
+
 				end
 
 			end
-			
+
 		end
 	end
 
 end
 
 local processConfig = function()
-	
-	preProcessAuras()	
+
+	preProcessAuras()
 	preProcessAlerts()
 
-	combineSpecTablesWithBase()	
+	combineSpecTablesWithBase()
 
 	postProcessAlerts()
 
