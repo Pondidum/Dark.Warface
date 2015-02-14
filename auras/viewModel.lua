13eea65c25e93d25bcdb3ecbe2add30dde73b01b
andy <andy@andydote.co.uk>
2015-02-14 22:50:41 +0000

    viewModel: update to use dark.class and dark.events

--- auras/viewModel.lua
+++ auras/viewModel.lua
@@ -1,8 +1,8 @@
 local addon, ns = ...
 local config = ns.config
 
-local core = Dark.core
---local events = core.events.new()
+local class = ns.lib.class
+local events = ns.lib.events
 local colors = ns.lib.media.colors
 
 local checkWhiteList = function(config, spellID)
@@ -13,125 +13,109 @@ local checkBlackList = function(config, spellID)
 	return not config[spellID]
 end
 
-local viewModel = {
 
-	new = function()
+local viewModel = class:extend({
 
-		local containers = ns.auras.viewController.new()
-		local specConfig
-		local classColor = {}
+	events = {
+		"ACTIVE_TALENT_GROUP_CHANGED",
+		"PLAYER_TARGET_CHANGED",
+		"PLAYER_FOCUS_CHANGED",
+		"UNIT_AURA",
+	},
 
-		local onSpecChanged = function()
+	ctor = function(self)
+		self:include(events)
 
-			local _, playerClass = UnitClass("player")
-			local spec = GetSpecialization()
+		self.containers = ns.auras.viewController.new()
+
+		self.containers.createViews(config.auraDisplays)
+		self:ACTIVE_TALENT_GROUP_CHANGED()
+
+	end,
+
+	ACTIVE_TALENT_GROUP_CHANGED = function(self)
+
+		local _, playerClass = UnitClass("player")
+		local spec = GetSpecialization()
 
 
-			local raidColor = colors.class[playerClass]
-
-			if spec then
-				local playerSpecID, playerSpec = GetSpecializationInfo(spec)
-				specConfig = config.classConfig[playerClass][playerSpec]
-			else
-				specConfig = setmetatable({}, { __index = config.specBase })
-			end
-
-			classColor = raidColor
-
+		local raidColor = colors.class[playerClass]
+		if spec then
+			local playerSpecID, playerSpec = GetSpecializationInfo(spec)
+			self.specConfig = config.classConfig[playerClass][playerSpec]
+		else
+			self.specConfig = setmetatable({}, { __index = config.specBase })
 		end
 
-		local onUnitAura = function(self, event, unit)
+		self.classColor = raidColor
 
-			local container = containers.getView(unit)
+	end,
 
-			if container == nil then
-				return
-			end
+	PLAYER_TARGET_CHANGED = function(self)
 
-			containers.resetView(unit)
+		self.containers.resetAllViews()
+		self:UNIT_AURA("target")
 
-			local auraConfig = specConfig.auras[unit]
+	end,
 
-			if auraConfig == nil then
-				return
-			end
+	PLAYER_FOCUS_CHANGED = function(self)
 
-			local check = auraConfig.mode == "WHITELIST" and checkWhiteList or checkBlackList
-			local children = {}
+		self.containers.resetAllViews()
+		self:UNIT_AURA("focus")
 
-			for i = 1, 20 do
+	end,
 
-				local auraName, auraRank, auraTexture, auraCount, auraDispel, auraDuration, auraExpires, caster, isStealable, shouldConsolidate, spellID = UnitAura(unit, i, auraConfig.filter)
+	UNIT_AURA = function(self, unit)
 
-				if auraName and auraDuration and auraDuration ~= 0 and auraExpires and auraExpires ~= 0  then
+		local container = self.containers.getView(unit)
 
-					if caster == "player" and check(auraConfig, spellID) then
+		if container == nil then
+			return
+		end
 
-						local view = container.getView(spellID)
+		self.containers.resetView(unit)
 
-						view.setColor(unpack(classColor))
-						view.setName(auraName)
-						view.setIcon(auraTexture)
-						view.setStacks(auraCount)
-						view.setCooldown(auraExpires - auraDuration, auraDuration)
+		local auraConfig = self.specConfig.auras[unit]
 
-						view:Show()
+		if auraConfig == nil then
+			return
+		end
 
-						table.insert(children, view)
-					end
+		local check = auraConfig.mode == "WHITELIST" and checkWhiteList or checkBlackList
+		local children = {}
 
+		for i = 1, 20 do
+
+			local auraName, auraRank, auraTexture, auraCount, auraDispel, auraDuration, auraExpires, caster, isStealable, shouldConsolidate, spellID = UnitAura(unit, i, auraConfig.filter)
+
+			if auraName and auraDuration and auraDuration ~= 0 and auraExpires and auraExpires ~= 0  then
+
+				if caster == "player" and check(auraConfig, spellID) then
+
+					local view = container.getView(spellID)
+
+					view.setColor(unpack(self.classColor))
+					view.setName(auraName)
+					view.setIcon(auraTexture)
+					view.setStacks(auraCount)
+					view.setCooldown(auraExpires - auraDuration, auraDuration)
+
+					view:Show()
+
+					table.insert(children, view)
 				end
 
 			end
 
-			table.sort(children, function(a, b) return a.remaining > b.remaining end)
-
-			container.children = children
-			container.performLayout()
-
 		end
 
-		local onTargetChanged = function()
+		table.sort(children, function(a, b) return a.remaining > b.remaining end)
 
-			containers.resetAllViews()
-			onUnitAura(nil, nil, "target")
-
-		end
-
-		local onFocusChanged = function()
-
-			containers.resetAllViews()
-			onUnitAura(nil, nil, "focus")
-
-		end
-
-		local registerEvents = function()
-
-			events.register("UNIT_AURA", onUnitAura)
-			events.register("PLAYER_TARGET_CHANGED", onTargetChanged)
-			events.register("PLAYER_FOCUS_CHANGED", onFocusChanged)
-
-		end
-
-
-		local this = {}
-
-		this.run = function()
-
-			containers.createViews(config.auraDisplays)
-			onSpecChanged()
-
-			registerEvents()
-
-		end
-
-		this.specChanged = function()
-			onSpecChanged()
-		end
-
-		return this
+		container.children = children
+		container.performLayout()
 
 	end
-}
+
+})
 
 ns.auras.viewModel = viewModel
--- run.lua
+++ run.lua
@@ -7,25 +7,18 @@ local warface = class:extend({
 
 	events = {
 		"PLAYER_ENTERING_WORLD",
-		"ACTIVE_TALENT_GROUP_CHANGED",
 	},
 
 	ctor = function(self)
 		self:include(events)
 
-		self.auras = ns.auras.viewModel.new()
 		Dark.warface = ns
-
 	end,
 
 	PLAYER_ENTERING_WORLD = function(self)
 		self:unregister("PLAYER_ENTERING_WORLD")
 
-		self.auras.run()
-	end,
-
-	ACTIVE_TALENT_GROUP_CHANGED = function(self)
-		self.auras.specChanged()
+		self.auras = ns.auras.viewModel:new()
 	end,
 })
 
